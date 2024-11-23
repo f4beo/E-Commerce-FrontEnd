3f2319a7ad54683f8fb08e935a95f7dc066e71fd
Arthur B <barbosarthur7@gmail.com>
2024-11-22 22:04:51 -0300

    adiciona função do seletor

--- cart/script.js
+++ cart/script.js
@@ -1,17 +1,12 @@
-// Função de diminui e aumentar os itens
-
-// Seleciona os botões e o contador
 const adiciona = document.querySelectorAll(".add");
 const diminui = document.querySelectorAll(".minus");
 
-// Função para adicionar 1 ao contador
 function adicionarUm(event) {
     const contador = event.target.closest('.botoes-produto').querySelector('.contador');
     let total = Number(contador.innerHTML) + 1;
     contador.innerHTML = total;
 }
 
-// Função para diminuir 1 do contador
 function diminuirUm(event) {
     const contador = event.target.closest('.botoes-produto').querySelector('.contador');
     let total = Number(contador.innerHTML) - 1;
@@ -20,11 +15,9 @@ function diminuirUm(event) {
     }
 }
 
-// Adiciona os event listeners aos botões de adicionar e diminuir
 adiciona.forEach(botao => botao.addEventListener("click", adicionarUm));
 diminui.forEach(botao => botao.addEventListener("click", diminuirUm));
 
-// Função do botão favoritar
 function alternarImagemSalvo(button) {
     const img = button.querySelector('img');
     if (img.src.includes('stash_save-ribbon-transparente')) {
@@ -41,3 +34,29 @@ document.querySelectorAll('.salvar-carrinho').forEach(button => {
         alternarImagemSalvo(button);
     });
 });
+
+const botaoSelecionarTipo = document.querySelectorAll('.Variador');
+
+botaoSelecionarTipo.forEach(button => {
+    button.addEventListener('click', function() {
+        const img = this.querySelector('img');
+        if (img) {
+            img.classList.toggle('move-right');
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const variadores = document.querySelectorAll('.Variador');
+    variadores.forEach(variador => {
+        variador.addEventListener('click', function() {
+            const imgProduto = this.closest('.caixa-info-produto').querySelector('.img-do-produto');
+            if (imgProduto.src.includes('sofaAlternativo.jpg')) {
+                imgProduto.src = './img/sofa 3.jpg';
+            } else {
+                imgProduto.src = './img/sofaAlternativo.jpg'; 
+            }
+        });
+    });
+});
+
